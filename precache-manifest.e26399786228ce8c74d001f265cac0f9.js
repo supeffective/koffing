c37553871573c4518dc7e8ac22de783379617258
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-08-29 00:08:51 +0200

    update gh-pages build

---
+++ precache-manifest.e26399786228ce8c74d001f265cac0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0567328fc80c175665a4923e85e87dff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b32fb586ddc6f4c08de",
+    "url": "/static/css/main.3ebdc5a1.chunk.css"
+  },
+  {
+    "revision": "9256987406dd851a6078",
+    "url": "/static/js/2.184985f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.184985f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b32fb586ddc6f4c08de",
+    "url": "/static/js/main.97b3484a.chunk.js"
+  },
+  {
+    "revision": "4732115756f3baa9add2",
+    "url": "/static/js/runtime-main.b3fc7813.js"
+  }
+]);
