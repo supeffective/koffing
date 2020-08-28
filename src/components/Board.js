cf3d75b83f485bdb48d266b4bf5d5ba9dbe36bff
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-08-29 01:51:43 +0200

    add QR code generation

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import QRCode from 'qrcode.react';
 import styled from '../styled';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -7,101 +8,121 @@ import Button from "@material-ui/core/Button";
 import Code from "./Code";
 import {Koffing} from "../core/Koffing";
 import exampleTeam from "../resources/example.koffing";
+import koffingImg from "../img/koffing.png";
+
+const qrIconSettings = {
+  src: koffingImg,
+  height: 32,
+  width: 32,
+  excavate: true
+}
 
 const styles = (theme) => ({
-    root: theme.mixins.gutters({
-        paddingTop: 16,
-        paddingBottom: 16,
-        marginTop: theme.spacing(),
-        flexGlow: 1
-    }),
-    paper: {
-        padding: theme.spacing() * 2,
-        color: theme.palette.text.secondary
-    },
-    textField: {
-        marginTop: 0,
-        overflow: 'hidden'
-    },
-    textArea: {
-        fontFamily: "monospace",
-        fontSize: "1em",
-        backgroundColor: theme.palette.secondary.main,
-        padding: theme.spacing()
-    }
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing(),
+    flexGlow: 1
+  }),
+  paper: {
+    padding: theme.spacing() * 2,
+    color: theme.palette.text.secondary
+  },
+  textField: {
+    marginTop: 0,
+    overflow: 'hidden'
+  },
+  textArea: {
+    fontFamily: "monospace",
+    fontSize: "1em",
+    backgroundColor: theme.palette.secondary.main,
+    padding: theme.spacing()
+  },
+  qrContainer: {
+    padding: theme.spacing() * 2,
+    textAlign: "center",
+    backgroundColor: "#dde0f6"
+  },
+  jsonContainer: {
+    padding: theme.spacing() * 2,
+    backgroundColor: "#fafafa"
+  }
 });
 
 class BoardComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: '', json: ''};
+  constructor(props) {
+    super(props);
+    this.state = {text: '', json: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    componentDidMount() {
-        this.update(exampleTeam);
-    }
+  componentDidMount() {
+    this.update(exampleTeam);
+  }
 
-    update(value) {
-        this.setState({
-            text: value,
-            json: Koffing.toJson(value)
-        });
-    }
+  update(value) {
+    this.setState({
+      text: value,
+      json: Koffing.toJson(value)
+    });
+  }
 
-    handleChange(event) {
-        this.update(event.target.value);
-    }
+  handleChange(event) {
+    this.update(event.target.value);
+  }
 
-    format() {
-        this.update(
-            Koffing.format(this.state.text) + "\n"
-        );
-    }
+  format() {
+    this.update(
+      Koffing.format(this.state.text) + "\n"
+    );
+  }
 
-    render() {
-        const {classes} = this.props;
-        return (
-            <div className={classes.root}>
-                <Grid container spacing={10}>
-                    <Grid item xs={12} sm={12} md={6}>
-                        <Paper className={classes.paper} elevation={1}>
-                            <TextField
-                                autoComplete="off"
-                                autoCorrect="off"
-                                autoCapitalize="off"
-                                spellCheck="false"
-                                multiline
-                                className={classes.textField}
-                                rows="12"
-                                rowsMax="24"
-                                autoFocus={true}
-                                placeholder="Place here your Showdown teams"
-                                // defaultValue={exampleTeam}
-                                fullWidth
-                                margin="normal"
-                                value={this.state.text}
-                                inputProps={{
-                                    className: classes.textArea + " koffing-in"
-                                }}
-                                onChange={this.handleChange}
-                            />
-                            <Button onClick={() => this.format()} variant={'contained'} color={'primary'}>Prettify</Button>
-                            &nbsp;<Button onClick={() => this.update('')} variant={'text'}>Clear</Button>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={6}>
-                        <Paper className={classes.paper} elevation={1}>
-                            <div className="koffing-out">
-                                <Code code={this.state.json} language={'json'}/>
-                            </div>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
+  render() {
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={10}>
+          <Grid item xs={12} sm={12} md={6}>
+            <Paper className={classes.paper} elevation={1}>
+              <TextField
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="off"
+                spellCheck="false"
+                multiline
+                className={classes.textField}
+                rows="12"
+                rowsMax="24"
+                autoFocus={true}
+                placeholder="Place here your Showdown teams"
+                // defaultValue={exampleTeam}
+                fullWidth
+                margin="normal"
+                value={this.state.text}
+                inputProps={{
+                  className: classes.textArea + " koffing-in"
+                }}
+                onChange={this.handleChange}
+              />
+              <Button onClick={() => this.format()} variant={'contained'} color={'primary'}>Prettify</Button>
+              &nbsp;<Button onClick={() => this.update('')} variant={'text'}>Clear</Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Paper className={classes.paper} elevation={1}>
+              <Paper className={classes.qrContainer}>
+                <QRCode value={this.state.text} size={320} renderAs="canvas" imageSettings={qrIconSettings}/>
+              </Paper>
+              <Paper className={classes.jsonContainer}>
+                <Code code={this.state.json} language={'json'} />
+              </Paper>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default styled(BoardComponent, styles);
--- src/components/Code.js
+++ src/components/Code.js
@@ -3,36 +3,37 @@ import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = {
-    root: {
-        margin: '6px 0'
-    },
-    pre: {
-        margin: 0,
-        border: 'none !important'
-    }
+  root: {
+    margin: '6px 0'
+  },
+  pre: {
+    margin: 0,
+    border: 'none !important',
+    color: '#008008'
+  }
 };
 
 class Code extends React.Component {
-    render() {
-        const formattedCode = this.props.code;
-        const codeClassName = `language-${this.props.language}`;
-        const {classes} = this.props;
-        return (
-            <div className={classes.root}>
-                <pre className={classes.pre + " prettyprint"}>
-                    <code className={codeClassName}>
-                        <div dangerouslySetInnerHTML={{__html: formattedCode}}/>
-                    </code>
-                </pre>
-            </div>
-        );
-    }
+  render() {
+    const formattedCode = this.props.code;
+    const codeClassName = `language-${this.props.language}`;
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <pre className={classes.pre}>
+          <code className={codeClassName}>
+              <div dangerouslySetInnerHTML={{__html: formattedCode}}/>
+          </code>
+        </pre>
+      </div>
+    );
+  }
 }
 
 Code.propTypes = {
-    classes: PropTypes.object.isRequired,
-    code: PropTypes.string.isRequired,
-    language: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  code: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(Code);
